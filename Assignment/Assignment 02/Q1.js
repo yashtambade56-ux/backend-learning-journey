15bc4578195bb729fd1f173eddd926d88b8e9d47
Yash tambade <yashtambade56@gmail.com>
2026-08-19 12:08:18 +0530

    Update Q1.js

--- Assignment/Assignment 02/Q1.js
+++ Assignment/Assignment 02/Q1.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Welcome to Node.js Server');
+    res.end('Welcome to Server');
 });
 
 server.listen(3000, () => {
